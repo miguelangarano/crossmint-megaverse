93b980dcbe69fdabf2524954ba66b586badbe08d
Miguel Langarano <miguelangarano@gmail.com>
2023-02-04 19:26:39 -0500

    async approach

--- src/models/Queue.ts
+++
@@ -1,9 +0,0 @@
-class Queue<T> {
-    _store: T[] = [];
-    push(val: T) {
-      this._store.push(val);
-    }
-    pop(): T | undefined {
-      return this._store.shift();
-    }
-  }
--- src/models/State.ts
+++ src/models/State.ts
@@ -1,18 +1,13 @@
 import { EventEmitter } from 'events';
-import { Astral } from './Astral';
 
 export class State {
     expectedAstralsNumber: number;
     createdAstralsNumber: number;
     eventEmitter: EventEmitter = new EventEmitter();
-    createdAstrals: Queue<Astral>;
-    erroredAstrals: Queue<Astral>;
 
     constructor(){
         this.expectedAstralsNumber = 0;
         this.createdAstralsNumber = 0;
-        this.createdAstrals = new Queue();
-        this.erroredAstrals = new Queue();
     }
 
     setExpectedAstrals(value: number){
