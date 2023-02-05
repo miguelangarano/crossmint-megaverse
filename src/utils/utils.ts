9c04418a2b3ae7e5f3ec0638328b011a29063541
Miguel Langarano <miguelangarano@gmail.com>
2023-02-04 20:27:13 -0500

    fix sleep

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -151,3 +151,7 @@ function handleAstralsDeleteQueueRequests(astral: Astral) {
         handleAstralDeletion(astral);
     }, getRandomNumber());
 }
+
+export function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
