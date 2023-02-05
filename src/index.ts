aef32b2d5dd82a2ac5884d35e562eec275d6a107
Miguel Langarano <miguelangarano@gmail.com>
2023-02-04 19:25:23 -0500

    queue

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,27 @@
+import { Queue } from 'async-await-queue';
 import dotenv from 'dotenv';
 import { CurrentMap, CurrentMapMatrix } from './models/CurrentMap';
 import { State } from './models/State';
-import { cleanComeths, cleanPolyanets, cleanSoloons, generateGoalMap, getRandomNumber } from './utils/utils';
+import { cleanComeths, cleanPolyanets, cleanSoloons, generateGoalMap, getRandomNumber, validateResponse } from './utils/utils';
 
 
 dotenv.config();
 let state: State;
 
-function init(){
+function init() {
+    const myq = new Queue(2, 500);
+    const myq2 = new Queue(1, 500);
     state = new State();
-    state.eventEmitter.on('astralAdded', () => {
+    state.eventEmitter.on('astralAdded', async () => {
         console.log('Data Received', state.createdAstralsNumber, state.expectedAstralsNumber);
-        if(state.createdAstralsNumber>=state.expectedAstralsNumber){
-            new CurrentMap().validateAnswer().then((solved)=>{
-                console.log("SOLVED::", solved)
-                if(solved==undefined){
-                    setTimeout(()=>{
-                        state.eventEmitter.emit('astralAdded');
-                    }, getRandomNumber());
-                    return;
-                }
-                if(solved){
-                    return;
-                }
-                generateGoalMap(state);
-            }).catch(()=>{
-                state.eventEmitter.emit('astralAdded');
-            })
+        if (state.createdAstralsNumber >= state.expectedAstralsNumber) {
+            await validateResponse(myq2, state, init);
         }
     });
-    generateGoalMap(state);
+    generateGoalMap(state, myq, init);
 }
 
-async function clean(){
+async function clean() {
     const currentMap: CurrentMap = new CurrentMap();
     const currentMapMatrix: CurrentMapMatrix = await currentMap.getCurrentMap();
     cleanSoloons(currentMapMatrix);
